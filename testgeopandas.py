6fd08790cd062f59a5cd6b17b5dcf86a9fa6d47b
collinkatz <collin.katz@gmail.com>
2022-07-21 16:14:22 -0400

    working on plotting and background

--- testgeopandas.py
+++ testgeopandas.py
@@ -1,5 +1,8 @@
 import geopandas as gpd
-minor_dataframe = gpd.read_file("./src/sidewalkdata/sidewalks_minor.shp")
-minor_series = minor_dataframe.head()
+import contextily as cx
+minor_dataframe = gpd.read_file("./src/sidewalkdata/sidewalks_minor.shp", rows=10)
+minor_series = minor_dataframe.get("geometry")
 minor_series = minor_series.to_crs(epsg=4326)  # EPSG:4326, EPSG:3857
-print(minor_series)
+# print(minor_series)
+ax = minor_series.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
+cx.add_basemap(ax)
