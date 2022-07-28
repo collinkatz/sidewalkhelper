90ae414dc8444b455d190685831e427b88f08db3
collinkatz <collin.katz@gmail.com>
2022-07-28 16:41:16 -0400

    got css stylesheet working, now working on map

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 // Imports
 const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+// const expressLayouts = require('express-ejs-layouts')
 
 // Start up app
 const app = express()
 const port = 3000
 
+// Set up static directorys to serve static data
+app.use(express.static('./src/public'))
+
 // Set view engine to load pages
-app.use(expressLayouts)
+// app.use(expressLayouts)
 app.set('views', './src/views');
+// app.set('layout', './src/layouts/main.ejs')
 app.set('view engine', 'ejs')
 
 // Page listeners
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index', {title: 'Map', layout: ''})
 })
 
 app.listen(port, () => {
