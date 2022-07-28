da9aa0aab388aa4514ca923f8190f6576c40db81
collinkatz <collin.katz@gmail.com>
2022-07-28 17:18:01 -0400

    map loading + working on getting python to call from javascript

--- testgeopandas.py
+++ testgeopandas.py
@@ -9,4 +9,4 @@ minor_series = minor_series.to_crs(epsg=4326)  # EPSG:4326, EPSG:3857
 print(minor_series)
 # ax = minor_series.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
 gmap = gmplot.GoogleMapPlotter(37.766956, -122.448481, 14, apikey=apikey)
-gmap.draw("/views/map.html")
+# gmap.draw("./map.html")
