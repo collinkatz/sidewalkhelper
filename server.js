8e4cb77a2f62629f899a5824eb0d11383090649c
Collin Katz <collin.katz@gmail.com>
2022-07-31 17:40:16 -0400

    added spawn for calling python

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 // Imports
-const express = require('express')
-// const expressLayouts = require('express-ejs-layouts')
+const express = require('express');
+// const expressLayouts = require('express-ejs-layouts');
+const { spawn } = require('node:child_process');
 
 // Start up app
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Set up static directorys to serve static data
-app.use(express.static('./src/public'))
+app.use(express.static('./src/public'));
 
 // Set view engine to load pages
 // app.use(expressLayouts)
 app.set('views', './src/views');
 // app.set('layout', './src/layouts/main.ejs')
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // Page listeners
 app.get('/', (req, res) => {
-  res.render('index', {title: 'Map', layout: ''})
-})
+  res.render('index', {title: 'Map', layout: ''});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
