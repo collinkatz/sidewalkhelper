7649dbc7a805631592a5896a9d16f2537f73e3e2
collinkatz <collin.katz@gmail.com>
2022-08-03 16:32:48 -0400

    added query response and json passing

--- dataQuery.py
+++ dataQuery.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 import json
+import sys
 from shapely.geometry import Point
 
 # Constants
@@ -16,11 +17,11 @@ class SHPQuery:
         SHPQuery is a class that allows for easy querying of spacial data files using bounding box coordinates
         :param query_id: An integer, this is a unique id number for the query
         :param county: A string, used to get the directory that holds the files for the county of interest
-        :param bound_coords: The top left and bottom right coordinates of a bounding box to query data within
+        :param bound_coords: The North-East and South-West coordinates of a bounding box to query data within
         :param crs: The preferred coordinate reference system id for the data to be returned in E.G. "epsg:4326"
         """
         try:
-            self.bounding_box = gpd.GeoDataFrame({'geometry':[Point(bound_coords.x1, bound_coords.y1), Point(bound_coords.x2, bound_coords.y2)]}, index=["p1", "p2"], crs={"init":crs})
+            self.bounding_box = gpd.GeoDataFrame({'geometry':[Point(bound_coords['x1'], bound_coords['y1']), Point(bound_coords['x2'], bound_coords['y2'])]}, index=["p1", "p2"], crs={"init":crs})
             # TODO: check if county exists in json and has data files - output error if not
             ctf_file_obj = open(county_to_file, 'r')
             self.file_names = json.load(ctf_file_obj)[county]
@@ -29,10 +30,10 @@ class SHPQuery:
             self.county_data_crs = self.get_crs_from_file(self.file_names[0])
             self.bounding_box = self.bounding_box.to_crs(self.county_data_crs)
             self.df = self.get_county_data().to_crs(crs)
-            self.output_json = self.df.to_file(output_dir + county + "_" + str(query_id), driver="GeoJSON")
-            print("query: " + county + "_" + query_id + " finished with status 0")
+            self.output_json = self.df.to_file(output_dir + str(query_id) + "_" + county, driver="GeoJSON")
+            print("query: " + query_id + "_" + county + " finished with status 0")
         except:
-            print("query: " + county + "_" + query_id + " exited with status -1")
+            print("query: " + query_id + "_" + county + " exited with status -1")
 
     def get_crs_from_file(self, file_name):
         temp_df = gpd.read_file(self.county_directory + file_name, rows=1)
@@ -44,3 +45,14 @@ class SHPQuery:
             file_df = gpd.read_file(self.county_directory + file_name, bbox=self.bounding_box)
             file_dfs.append(file_df)
         return pd.concat(file_dfs)
+
+if __name__ == "__main__":
+    args = sys.argv
+    try:
+        qid = args[1]
+        ne = json.loads(args[2])
+        sw = json.loads(args[3])
+        county = args[4]
+        SHPQuery(qid, county, {'x1': ne["lng"], 'y1': ne["lat"], 'x2': sw["lng"], 'y2': sw["lat"]}, maps_crs)
+    except IndexError:
+        print("Not enough arguments")
