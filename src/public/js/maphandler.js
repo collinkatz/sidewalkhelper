6b04841baa46ef9107cc9c88e4c3111940e5a911
collinkatz <collin.katz@gmail.com>
2022-08-04 16:36:07 -0400

    a lot of changes, fixed async stuff, now sends post request with query args in request body

--- src/public/js/maphandler.js
+++ src/public/js/maphandler.js
@@ -1,22 +1,31 @@
-const async = require('async')
-
 async function initMap(county) {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 39.29360, lng: -76.81646 }, // TODO: need to change these to update based on user location
         zoom: 16,
     });
-    async function getMapBounds() {
-        return await google.maps.event.addListenerOnce(map, 'idle', async function () {
-            northEast = map.getBounds().getNorthEast().toString()
-            southWest =  map.getBounds().getSouthWest().toString()
-            console.log("ne:" + northEast + " sw: " + southWest)
-            return {'ne': northEast, 'sw': southWest}
+    google.maps.event.addListenerOnce(map, "idle", async function () {
+        var bounds = await mapBounds()
+        console.log("ne: (lat: " + bounds["ne"]["lat"] + ", lng: " + bounds["ne"]["lng"] + ") sw: (lat: " + bounds["sw"]["lat"] + ", lng: " + bounds["sw"]["lng"] + ")")
+        var response = await fetch("/queryCountyData", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "ne": bounds["ne"],
+                "sw": bounds["sw"],
+                "county": county
+            })
         });
-    };
-    var bounds = getMapBounds()
-    console.log(bounds)
-    let response = await fetch('/queryCountyData?northEast=' + bounds['ne'] + '&southWest=' + bounds['sw'] + '&county=' + county) // TODO: Modify this fetch request to be better using method: and body: and headers: Also needs rejected promise handling??
-    let responseGeoJSON = await response.json()
-    map.data.loadGeoJson(responseGeoJSON)
-    console.log(responseGeoJSON)
+        console.log(response)
+        var responseGeoJSON = await response.json()
+        console.log(responseGeoJSON)
+        map.data.loadGeoJson(responseGeoJSON) // TODO: Needs rejected promise handling??
+        console.log(fromServer)
+    });
+    async function mapBounds() {
+        northEast = map.getBounds().getNorthEast()
+        southWest =  map.getBounds().getSouthWest()
+        return {"ne": {"lat": northEast.lat(), "lng": northEast.lng()},"sw": {"lat": southWest.lat(), "lng": southWest.lng()}}
+    }
 }
