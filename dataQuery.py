57fab965b24b8c8863bef043c937199101859648
Collin Katz <collin.katz@gmail.com>
2022-08-21 17:33:05 -0400

    commented out print

--- dataQuery.py
+++ dataQuery.py
@@ -11,7 +11,7 @@ try:
     bounds = json.loads(args[2].replace("'", "\"")) # Need to take single quotes and turn them into double quotes for json.loads
     county = args[3]
     # TODO: Check that boundcoords are not null
-    print("python query: " + qid + str(bounds["ne"]) + str(bounds["sw"]) + county)
+    # print("python query: " + qid + str(bounds["ne"]) + str(bounds["sw"]) + county)
     SHPQuery(qid, county, bounds, maps_crs)
 except IndexError:
     print("Not enough arguments")
