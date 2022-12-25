ab68a2e0d04dc7e6555e023b6e7764b0a2f3926b
Collin Katz <collin.katz@gmail.com>
2022-12-25 18:55:26 -0500

    exec instead of spawn so we can get process stdout

--- ws-server.js
+++ ws-server.js
@@ -3,7 +3,7 @@ const port = require("./http-server").port;
 const expressWs = require('express-ws');
 const fs = require('fs');
 
-const { spawn } = require('node:child_process');
+const { exec } = require('node:child_process');
 const { unlink } = require('node:fs');
 const path = require('node:path');
 
@@ -34,29 +34,31 @@ app.ws('/mapws', (ws, req) => {
       myQID = queryCount;
       //TODO cancel process if new query comes in
       // Spawn python subprocess to make queried file
-      var process = spawn('python', ['./dataQuery.py', myQID, bounds, county], {shell: true});
-      console.log("spawned process")
-      process.stderr.on('data', (data) => { // Outputs errors to command line
-        console.log(String(data))
+      var process = exec(`python ./dataQuery.py ${myQID} ${bounds} ${county}`, (error, stdout, stderr) => {
+        ws.send(stdout);
       });
+      // console.log("spawned process")
+      // process.stderr.on('data', (data) => { // Outputs errors to command line
+      //   console.log(String(data))
+      // });
 
       // When process outputs send file and then remove queried json
-      process.stdout.on('data', (data) => { // Called when python query outputs exit status
-        console.log(String(data))
-        var myGeoJSONFile = './query_output/' + myQID + '_' + county + '.json';
-        var geoJSONData = fs.readFileSync(myGeoJSONFile, 'utf8');
-        // JSON.parse(rawdata);
-        // ERROR HERE need to use ws.emit instead of res.sendFile
-        ws.send(geoJSONData);
-        // res.on('finish', () => { // Finish used to delete file after successfully sending
-        //   try {
-        //     unlink(myGeoJSONFile, () => {console.log("Deleted query json")}); 
-        //   } catch(e) {
-        //     console.log("error removing ", myGeoJSONFile);
-        //   }
-        // });
-        // console.log(myGeoJSONFile);
-      });
+      // process.stdout.on('data', (data) => { // Called when python query outputs exit status
+      //   console.log(String(data))
+      //   var myGeoJSONFile = './query_output/' + myQID + '_' + county + '.json';
+      //   var geoJSONData = fs.readFileSync(myGeoJSONFile, 'utf8');
+      //   // JSON.parse(rawdata);
+      //   // ERROR HERE need to use ws.emit instead of res.sendFile
+      //   ws.send(geoJSONData);
+      //   // res.on('finish', () => { // Finish used to delete file after successfully sending
+      //   //   try {
+      //   //     unlink(myGeoJSONFile, () => {console.log("Deleted query json")}); 
+      //   //   } catch(e) {
+      //   //     console.log("error removing ", myGeoJSONFile);
+      //   //   }
+      //   // });
+      //   // console.log(myGeoJSONFile);
+      // });
     } catch(e) {
       console.log(e);
     }
