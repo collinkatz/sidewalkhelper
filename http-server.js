94ce0f7b00c6615b481bc4de73bbe31fac45f104
Collin Katz <collin.katz@gmail.com>
2022-12-26 22:32:02 -0500

    no need for post request now

--- http-server.js
+++ http-server.js
@@ -25,38 +25,54 @@ app.get('/', (req, res) => {
   
 });
 
-app.post('/queryCountyData', (req, res) => { // Depricated
+// app.post('/queryCountyData', (req, res) => {
+//   try {
+//     // Parse request and update query count
+//     queryCount++;
+//     myQID = queryCount;
+//     bounds = JSON.stringify(req.body['bounds']).replaceAll('\"', "\'"); // Need to replace double quotes with single for command line input
+//     county = req.body['county'];
 
-  // Parse request and update query count
-  queryCount++;
-  myQID = queryCount;
-  bounds = JSON.stringify(req.body['bounds']).replaceAll('\"', "\'"); // Need to replace double quotes with single for command line input
-  county = req.body['county'];
+//     var queryProcess = exec(`python ./dataQuery.py ${myQID} ${bounds} ${county}`, (error, stdout, stderr) => {
+//       res.send(stdout);
+//     });
+//   } catch(e) {
+//     console.log(e);
+//   }
+// });
 
-  //TODO cancel process if new query comes in
-  // Spawn python subprocess to make queried file
-  var process = spawn('python', ['./dataQuery.py', myQID, bounds, county], {shell: true});
-  console.log("spawned process")
-  process.stderr.on('data', (data) => { // Outputs errors to command line
-    console.log(String(data))
-  });
+// app.post('/queryCountyData', (req, res) => { // Depricated
 
-  // When process outputs send file and then remove queried json
-  process.stdout.on('data', (data) => { // Called when python query outputs exit status
-    console.log(String(data))
-    var myGeoJSONFile = './query_output/' + myQID + '_' + county + '.json';
-    res.sendFile(myGeoJSONFile, {root: path.join(__dirname)});
-    res.on('finish', () => { // Finish used to delete file after successfully sending
-      try {
-        unlink(myGeoJSONFile, () => {console.log("Deleted query json")}); 
-      } catch(e) {
-        console.log("error removing ", myGeoJSONFile); 
-      }
-    });
-    console.log(myGeoJSONFile);
-  });
+//   // Parse request and update query count
+//   queryCount++;
+//   myQID = queryCount;
+//   bounds = JSON.stringify(req.body['bounds']).replaceAll('\"', "\'"); // Need to replace double quotes with single for command line input
+//   county = req.body['county'];
 
-});
+//   //TODO cancel process if new query comes in
+//   // Spawn python subprocess to make queried file
+//   var process = spawn('python', ['./dataQuery.py', myQID, bounds, county], {shell: true});
+//   console.log("spawned process")
+//   process.stderr.on('data', (data) => { // Outputs errors to command line
+//     console.log(String(data))
+//   });
+
+//   // When process outputs send file and then remove queried json
+//   process.stdout.on('data', (data) => { // Called when python query outputs exit status
+//     console.log(String(data))
+//     var myGeoJSONFile = './query_output/' + myQID + '_' + county + '.json';
+//     res.sendFile(myGeoJSONFile, {root: path.join(__dirname)});
+//     res.on('finish', () => { // Finish used to delete file after successfully sending
+//       try {
+//         unlink(myGeoJSONFile, () => {console.log("Deleted query json")}); 
+//       } catch(e) {
+//         console.log("error removing ", myGeoJSONFile); 
+//       }
+//     });
+//     console.log(myGeoJSONFile);
+//   });
+
+// });
 
 // Start listening
 // app.listen(port, () => {
